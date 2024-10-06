9a3a58d7c6faf4747d5abe30b3ea343b771f952b
willamtessa27 <williamtessa270794@gmail.com>
2024-10-06 15:31:24 +0200

    change Url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-	baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api/v1" : "/api/v1",
+	// baseURL: "http://localhost:5000/api/v1",
+	baseURL: "https://clone-linkedlin.onrender.com/api/v1",
 	withCredentials: true,
 });
